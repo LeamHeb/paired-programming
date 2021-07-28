b8117599dfffb62083795791dc76b1661d9834ee
Leam Delaney <delaney.leam@heb.com>
2021-07-28 10:55:26 -0500

    Adding static hamcrest import

--- src/test/java/com/heb/pairedprogramming/stringreversal/StringReversalController_IntegrationTest.java
+++ src/test/java/com/heb/pairedprogramming/stringreversal/StringReversalController_IntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.is;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
